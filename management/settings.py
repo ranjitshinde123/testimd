7d245e20bae8fac8fa73036cfc242f68e8dad830
komal <komalp.mobicloud@gmail.com>
2023-02-06 15:01:15 +0530

    second commit

--- management/settings.py
+++ management/settings.py
@@ -353,14 +353,23 @@ WSGI_APPLICATION = 'management.wsgi.application'
 # api_key='e914114c-2d0c-11ed-9c12-0200cd936042'
 # # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'demodb',
+        'USER': 'demo1',
+        'PASSWORD': 'imd@123',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
