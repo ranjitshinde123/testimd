a4bcafc6a8075ea813846230aaac6b6c240b73bf
komal <komalp.mobicloud@gmail.com>
2023-02-06 14:29:44 +0530

    first commit

--- management/urls.py
+++ management/urls.py
@@ -35,8 +35,8 @@ urlpatterns = [
 
 
 
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    # path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 
     # path('', include('homepage.urls')),
 
